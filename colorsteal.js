581f6de55be5dea8302144d234d87f137bb74a07
ccjt <circjit@gmail.com>
2025-04-02 22:17:16 -0300

    Update colorsteal.js

--- colorsteal.js
+++ colorsteal.js
@@ -8,9 +8,8 @@
 // @icon         data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsSAAALEgHS3X78AAAAAXNSR0IArs4c6QAAAnpJREFUeF7tmlFShDAMhrfjDdRX9XLO6JH0ePqsZ8AJM2FiJ23DJiTAxhdZKIV8Tf4kQJmmabrc8F9JAOkBGQKpATesgZcUwcwCnSxQSmGj40yJoxkCYPyZDG3p3KYAjgAxAbRE0GL1LObYOkWrPaAWSqobNQDJWBzjpT8qANwK032tbVzV+jjs9zJ8uQdNCFgD8DYeIOzKAxJAQGPa7QUklaBE2GjMU1XvCebW6j/UAK8biL5OdoOR3SAXYt5CGOYBEVUim7ajPCABBLTbh/QAS0/ZFYC5DGWeONW1AaZJ3N+rO7g5R7VGmAg2n9BUobG2oaqzyPB8rQj+fn4Pa5mHt+fhGK5DxBVtrT7nHa4AJMbjTUohcM8QKIBenbCmO1WXwmg8GAbb+L+31BIIhwMABnPGc2BqACMRrENgrXCKQuRaDaDuTwG0thGUWAycBqqywBYa4GT3chkVAJhFAuHx/eWfXd4NTw+qGoBkxUbFi2SOrca4A4DVtyxvtWBcAfx8fM33C0K5FwguAGiVtzcIrgAwjyME+A0CGSmKIQDAcIQAACiQUUxLqsnRHPS4OwBMnZga1xi/tq+QgHAHIKkbRjde7srl/vVpNEx0PBwAunTdXLV6DGsvCAUwaqLQWM5rrLRgtwBO6wF1/0BDoPaI1jMGq9Wf0/K17bBIYTqDrhVDS+NDAWgBWp0f5gFWBmjnSQBRGqBdOavz0wPSAyJ7USs/VsyTIZAhkCHQ/jqx9bIRQu4s3LqfytaGjt61K7Qo7FQWABqqeVMbZtHKC88AJAbPnRP5emMvz/Wl9nKf5i/d4C0BqG1dQqD3dQVH70giiO8m6Ws5tOkPIuP6YibZoUkAAAAASUVORK5CYII=
 // @grant        none
 // ==/UserScript==
-let resetname = MPP.client.ppl[MPP.client.participantId].name
-let resetcolor = MPP.client.ppl[MPP.client.participantId].color
-let help = true
+let resetname = "ccjt"
+let resetcolor = "#b3acf1"
 // "u": "n", derwear haha gottem
 // (^preserve^)
 MPP.client.on('a', function(m) {
@@ -19,61 +18,63 @@ MPP.client.on('a', function(m) {
     let randomhex = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
     // cmds
     if (m.p.id == MPP.client.participantId) {
-        if (cmd == "help" && help) {
-            if (args.length == 0) {
+        if (cmd == "help") {
+            if (args.length == 1) {
                 MPP.chat.send("Please choose a category: userset, info, fun, other")
-            } else if (args[1] == "userset") {
-                MPP.chat.send("Commands: steal - steals color from ID | color - sets color to hex | name - sets name | shuffle - makes you a random color | reset - resets you to your defaults | stat - this command sets a status for you")
-            } else if (args[1] == "info") {
-                MPP.chat.send("Commands: mycolor - tells you your current color | settings - logs room settings to console | about - info about bot OR says info about user - Usage: about - tells you bot info ; about [ID] - tells you info about user | help - lists commands OR lists info about command - Usage: help - tells you possible commands ; help usage [command name] - tells you command usage | define - defines a variable. | whereami - tells you the room name.")
-            } else if (args[2] == "fun") {
+            } else if (args.length == 2) {
+                if (args[1] == "userset") {
+                    MPP.chat.send("Commands: steal - steals color from ID | color - sets color to hex | name - sets name | shuffle - makes you a random color | reset - resets you to your defaults | stat - this command sets a status for you")
+                } else if (args[1] == "info") {
+                    MPP.chat.send("Commands: mycolor - tells you your current color | settings - logs room settings to console | about - info about bot OR says info about user - Usage: about - tells you bot info ; about [ID] - tells you info about user | help - lists commands OR lists info about command - Usage: help - tells you possible commands ; help usage [command name] - tells you command usage | define - defines a variable. | whereami - tells you the room name.")
+                } else if (args[2] == "fun") {
                     MPP.chat.send("Commands: flip - flips or fails")
-            } else if (args[2] == "other") {
+                } else if (args[2] == "other") {
                     MPP.chat.send("Commands: fave - favorites an item | faves - tells you favorited items | wipefaves - erases favorited items | favestat - favorites a status | wipestats - wipes favorited stats |")
-            } else if (args[1] == "usage") {
-                if (args.length == 1) {
-                    MPP.chat.send("Please specify a command to know about. Example: help usage steal")
-                } else if (args[2] == "steal") {
-                    MPP.chat.send("Steal - This command takes the color from the ID you specify and sets your color to it. - Example: steal [ID]")
-                } else if (args[2] == "color") {
-                    MPP.chat.send("Color - This command sets your color to the hex code you specify. - Example: color #bababa - This example command sets your color to Baby Talk Grey.")
-                } else if (args[2] == "name") {
-                    MPP.chat.send("Name - This command sets your name to the text you specify. - Example: name Anonymous is using colorsteal - This example command sets your name to \"Anonymous is using colorsteal\".")
-                } else if (args[2] == "shuffle") {
-                    MPP.chat.send("Shuffle - This command sets your color to a completely random color. - Example: shuffle - This example sets your color to a random color that you've probably never seen before.")
-                } else if (args[2] == "reset") {
-                    MPP.chat.send("Reset - This command resets your name and color to your default name and color. You can change your reset name and color by doing \"define reset name [name]\" and \"define reset color [hex code]\" respectively. - Example: reset")
-                } else if (args[2] == "mycolor") {
-                    MPP.chat.send("MyColor - This command tells you your current color in hex. - Example: mycolor - This example command tells you your current color.")
-                } else if (args[2] == "settings") {
-                    MPP.chat.send("Settings - This command logs the current room settings in your DevTools console. You can access it by pressing F12 or CTRL+Shift+I and clicking on \"Console\" on the tabs on the top of the menu that opens.")
-                } else if (args[2] == "about") {
-                    MPP.chat.send("About - This command tells you bot info when no ID is provided, but if you specify an ID, it will tell you the info about that user. - Example 1: about ; This example sends a message with bot info. - Example 2: about [ID] ; This example sends a message with the info about the specified ID.")
-                } else if (args[2] == "help") {
-                    MPP.chat.send("Help - This command displays the list of commands in a category, or tells you the usage of a command. - Example 1: help info; This example shows the commands in the \"info\" category. - Example 2: help usage steal ; This command shows the usage of the \"steal\" command.")
-                } else if (args[2] == "define") {
-                    MPP.chat.send("Define - This command defines a variable. - Example: define reset name Anonymous | This example command sets your reset name to Anonymous, so that when you use the \"reset\" command, your name is set to \"Anonymous\".")
-                } else if (args[2] == "whereami") {
-                    MPP.chat.send("WhereAmI - This command says your current room name. - Example: whereami - This example command says that \"You're in the room \"" + MPP.client.channel.id + "\".\"")
-                } else if (args[2] == "stat") {
-                    MPP.chat.send("Stat - This command adds a status at the end of your name. - Example: stat AFK - This example command sets your name to \"" + m.p.name + " [AFK]\".")
-                } else if (args[2] == "flip") {
-                    MPP.chat.send("Flip - This command has a 69% chance of saying \"*flips*\", and a 31% chance of saying \"*fails*\".")
-                } else if (args[2] == "fave") {
-                    MPP.chat.send("Fave - This command favorites an item. - Example: fave #bababa - baby gray - This example command favorites the string \"#bababa - baby gray\". You can check your favorited items with \"faves\".")
-                } else if (args[2] == "faves") {
-                    MPP.chat.send("Faves - This command tells you your favorited items.")
-                } else if (args[2] == "wipefaves") {
-                    MPP.chat.send("WipeFaves - This command erases all your favorited items permanently, with no way to bring them back.")
-                } else if (args[2] == "favestat") {
-                    MPP.chat.send("FaveStat - This command favorites a status. This command is prone to deletion because you can also use the \"fave\" command for the same effect.")
-                } else if (args[2] == "wipestats") {
-                    MPP.chat.send("WipeStats - This command wipes all of your favorited statuses, with no way to recover them.")
+                } else if (args[1] == "usage") {
+                    if (args.length == 2) {
+                        MPP.chat.send("Please specify a command to know about. Example: help usage steal")
+                    } else if (args[2] == "steal") {
+                        MPP.chat.send("Steal - This command takes the color from the ID you specify and sets your color to it. - Example: steal [ID]")
+                    } else if (args[2] == "color") {
+                        MPP.chat.send("Color - This command sets your color to the hex code you specify. - Example: color #bababa - This example command sets your color to Baby Talk Grey.")
+                    } else if (args[2] == "name") {
+                        MPP.chat.send("Name - This command sets your name to the text you specify. - Example: name Anonymous is using colorsteal - This example command sets your name to \"Anonymous is using colorsteal\".")
+                    } else if (args[2] == "shuffle") {
+                        MPP.chat.send("Shuffle - This command sets your color to a completely random color. - Example: shuffle - This example sets your color to a random color that you've probably never seen before.")
+                    } else if (args[2] == "reset") {
+                        MPP.chat.send("Reset - This command resets your name and color to your default name and color. You can change your reset name and color by doing \"define reset name [name]\" and \"define reset color [hex code]\" respectively. - Example: reset")
+                    } else if (args[2] == "mycolor") {
+                        MPP.chat.send("MyColor - This command tells you your current color in hex. - Example: mycolor - This example command tells you your current color.")
+                    } else if (args[2] == "settings") {
+                        MPP.chat.send("Settings - This command logs the current room settings in your DevTools console. You can access it by pressing F12 or CTRL+Shift+I and clicking on \"Console\" on the tabs on the top of the menu that opens.")
+                    } else if (args[2] == "about") {
+                        MPP.chat.send("About - This command tells you bot info when no ID is provided, but if you specify an ID, it will tell you the info about that user. - Example 1: about ; This example sends a message with bot info. - Example 2: about [ID] ; This example sends a message with the info about the specified ID.")
+                    } else if (args[2] == "help") {
+                        MPP.chat.send("Help - This command displays the list of commands in a category, or tells you the usage of a command. - Example 1: help info ; This example shows the commands in the \"info\" category. - Example 2: help usage steal ; This command shows the usage of the \"steal\" command.")
+                    } else if (args[2] == "define") {
+                        MPP.chat.send("Define - This command defines a variable. - Example: define reset name Anonymous | This example command sets your reset name to Anonymous, so that when you use the \"reset\" command, your name is set to \"Anonymous\".")
+                    } else if (args[2] == "whereami") {
+                        MPP.chat.send("WhereAmI - This command says your current room name. - Example: whereami - This example command says that \"You're in the room \"" + MPP.client.channel.id + "\".\"")
+                    } else if (args[2] == "stat") {
+                        MPP.chat.send("Stat - This command adds a status at the end of your name. - Example: stat AFK - This example command sets your name to \"" + m.p.name + " [AFK]\".")
+                    } else if (args[2] == "flip") {
+                        MPP.chat.send("Flip - This command has a 69% chance of saying \"*flips*\", and a 31% chance of saying \"*fails*\".")
+                    } else if (args[2] == "fave") {
+                        MPP.chat.send("Fave - This command favorites an item. - Example: fave #bababa - baby gray - This example command favorites the string \"#bababa - baby gray\". You can check your favorited items with \"faves\".")
+                    } else if (args[2] == "faves") {
+                        MPP.chat.send("Faves - This command tells you your favorited items.")
+                    } else if (args[2] == "wipefaves") {
+                        MPP.chat.send("WipeFaves - This command erases all your favorited items permanently, with no way to bring them back.")
+                    } else if (args[2] == "favestat") {
+                        MPP.chat.send("FaveStat - This command favorites a status. This command is prone to deletion because you can also use the \"fave\" command for the same effect.")
+                    } else if (args[2] == "wipestats") {
+                        MPP.chat.send("WipeStats - This command wipes all of your favorited statuses, with no way to recover them.")
+                    }
                 }
             }
         }
         if (cmd == "fave") {
-            if (args.length == 0) {
+            if (args.length == 1) {
                 localStorage.setItem("fave", localStorage.fave + ", " + m.p.color)
                 MPP.chat.send('faved!')
             } else {
@@ -89,7 +90,7 @@ MPP.client.on('a', function(m) {
             }
         }
         if (cmd == "wipefaves") {
-            if (args.length == 0) {
+            if (args.length == 1) {
                 MPP.chat.send("ARE YOU SURE? [y/n]")
             } else {
                 if (args[1] == "y") {
@@ -104,7 +105,7 @@ MPP.client.on('a', function(m) {
             MPP.chat.send(localStorage.stat)
         }
         if (cmd == "stat") {
-            if (m.a.substring(4).trim().length + m.p.name + 2 > 39) {
+            if (m.a.substring(4).trim().length + m.p.name + 2 > 40) {
                 MPP.chat.send("stat too long!! (final name length: " + (m.a.substring(4).trim().length + m.p.name.length + 2) + ")")
             } else {
                 MPP.chat.send("name " + m.p.name + " [" + m.a.substring(4).trim() + "]")
@@ -161,11 +162,11 @@ MPP.client.on('a', function(m) {
             console.log(JSON.stringify(MPP.client.channel.settings))
         }
         if (cmd == "define") {
-            if (args.length == 0) {
+            if (args.length == 1) {
                 MPP.chat.send("Please insert a category to define. Categories: reset, help")
             } else {
                 if (args[1] == "reset") {
-                    if (args.length == 1) {
+                    if (args.length == 2) {
                         MPP.chat.send("Please insert a variable to define. Variables: name, color")
                     } else {
                         if (args[2] == "name") {
@@ -175,14 +176,14 @@ MPP.client.on('a', function(m) {
                             let resetcolor = `${m.a.substring(18).trim()}`
                             MPP.chat.send("Your reset color is now " + resetcolor + ".")
                         } else if (args[2] == "default") {
-                            let resetname = `${m.a.substring(17).trim()}`
-                            MPP.chat.send("Your reset name is now " + resetname + ".")
-                            let resetcolor = `${m.a.substring(18).trim()}`
-                            MPP.chat.send("Your reset color is now " + resetcolor + ".")
+                            let resetname = `Anonymous`
+                            MPP.chat.send("Your reset name is now Anonymous.")
+                            let resetcolor = randomhex
+                            MPP.chat.send("Your reset color is now " + randomhex + ".")
                         }
                     }
-                } else if (args[1] == "help") {
-                    if (args.length == 1) {
+                /*} else if (args[1] == "help") {
+                     if (args.length == 1) {
                         MPP.chat.send("Please insert a variable to define. Variables: on, off")
                     } else {
                         if (args[2] == "on") {
@@ -200,7 +201,7 @@ MPP.client.on('a', function(m) {
                                 MPP.chat.send("Help command is already off.")
                             }
                         }
-                    }
+                    }*/
                 }
             }
         }
